db98e35ef4113aa305d1db6fa90dfb545695d020
Ping <theping-1@outlook.com>
2023-09-27 15:38:41 +0700

    Assign Complete

--- ex-4.js
+++ ex-4.js
@@ -1,4 +1,18 @@
 // Exercise #4 Grouping Function with Objects
 
 // Start coding here
-let calculator = {};
+let add = ( a , b) => a + b;
+let subtract = ( a , b ) => a - b;
+let multiply = ( a , b ) => a * b;
+let divide = ( a , b ) => a / b;
+
+let calculator = { add: add,
+                    subtract: subtract,
+                    multiply: multiply,
+                    divide: divide  };
+                
+let resaultAdd = calculator.add( 10 , 20 )
+console.log(resaultAdd)
+
+let resaultDivide = calculator.divide( 3000 , 10 )
+console.log(resaultDivide)
